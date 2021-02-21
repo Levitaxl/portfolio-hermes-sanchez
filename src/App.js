3b0feb554ddb85dec75d850e4599ad65dc0b4267
levitaxl <hermessamuel10@gmail.com>
2021-02-21 17:49:07 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import {MyNavbar}  from './components/mynavbar/mynavbar.component';
+//Components
+
+
 
 const  App =  () => {
-   return <div className='App'>My portfolio</div>
+   return (
+      <div className='App'>
+         <MyNavbar />
+         My portfolio
+      </div>);
 }
 
 export default App;
