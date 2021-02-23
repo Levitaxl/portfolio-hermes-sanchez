2aa296fb260b17cfdd4b8486142a93b8737bbd66
levitaxl <hermessamuel10@gmail.com>
2021-02-22 18:25:36 -0600

    carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import {MyNavbar}  from './components/mynavbar/mynavbar.component';
+import {MyCarousel} from './components/mycarousel/mycarousel.component';
+import {TitleMessage} from './components/title-message/title-message.component'
 //Components
 
 
@@ -8,7 +10,7 @@ const  App =  () => {
    return (
       <div className='App'>
          <MyNavbar />
-         My portfolio
+         <MyCarousel />
       </div>);
 }
 
