a69dbc11bb4f6f133c017e91ad60f564b8a4b7fb
levitaxl <hermessamuel10@gmail.com>
2021-03-14 13:00:16 -0600

    actualización de la experiencia

--- src/pages/experience/experience.component.jsx
+++ src/pages/experience/experience.component.jsx
@@ -22,22 +22,15 @@ export const Experience = () => {
                   <Card.Text className="text-center style">
                     <strong className="body-title-style ">Full Stack Developer</strong>
                     <br />
-                    <strong>Technologies:</strong> PHP, CODEIGNITER, HTML, CSS, JS, Jquery, React JS,Mysql  
+                    <strong>Technologies:</strong> PHP, CODEIGNITER, HTML, CSS, JS, Jquery, React,Mysql  
                     <br />
-                    <strong>Duration:</strong> June 2020 - Present
+                    <strong>Duration:</strong> <br /> June 2020 - Present
                     <br/>
                     <strong> Description </strong>
                     <ul className="text-left">
-                      <li><strong>Developed &amp; enhanced</strong> multiple features with customizability option across web application.</li>
-                      <li><strong>Developed</strong> automation system to create SQL bulk query scripts that 
-                        increased efficiency by 80% and decreased working hours from 4 hours to 30 mins per task.
-                      </li>
-                      <li><strong>Provided</strong> application maintenance while working as `Production Support`. 
-                      </li>
-                      <li><strong>Performed</strong> CRUD operations on multiple databases to load/ remove data according 
-                      to the business requirements.</li>
-                      {/* <li><strong>Co-created</strong> React Document used as a guide for new developers.</li> */}
-
+                      <li>Creation of several web pages using wordpress as cms</li>
+                      <li>Creation of web pages and plugins using Perfex CRM</li>
+                      <li>Using php with frameworks (Laravel, Codeigniter) to create web applications</li>
                     </ul>
                     
                     
